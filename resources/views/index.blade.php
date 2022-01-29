802341dc436821afd1b9b5268d13c24c4561a8f0
Musa Avcı <strefrextor@gmail.com>
2022-01-29 23:33:15 +0300

    Links updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@
         </header>
         <div class="small alert alert-warning">This is a third-party, unofficial, open-source service and not affiliated with Hacker News or YC.</div>
         <main>
-            <p>Hacker News is part of my daily life. I try to follow the top stories every day when I have the opportunity. In order not to miss important stories on my busy days, I prepared a notification service. It was a very simple, 43-line PHP script that sends me notifications for stories with over 200 points. I have been using this service for 7 months and I no longer worry about missing important stories.</p>
+            <p><a target="_blank" href="https://news.ycombinator.com">Hacker News</a> is part of my daily life. I try to follow the top stories every day when I have the opportunity. In order not to miss important stories on my busy days, I prepared a notification service. It was a very simple, <a target="_blank" href="https://github.com/mavci/hn-notifications/blob/main/legacy.php.txt">~40-line PHP script</a> that sends me notifications for stories with over 200 points. I have been using this service for 7 months and I no longer worry about missing important stories.</p>
 
             <p>Finally, I made this service available to everyone so that it can be useful to others. I have also obtained the necessary permissions from the HN moderators to share such a service with you. So, I hope you will not miss important stories from this awesome platform with the help of this service. Also when I share this service with HN, I hope I will receive this story as a notification.</p>
             <div class="card bg-light">
@@ -106,7 +106,7 @@
             </div>
         </main>
         <footer class="pt-5 text-muted border-top">
-            <a href="https://github.com/mavci/hn-notifications" target="_blank">GitHub</a> &middot; 2022
+            <a href="https://github.com/mavci/hn-notifications" target="_blank">GitHub Repository</a> &middot; 2022
         </footer>
     </div>
 
